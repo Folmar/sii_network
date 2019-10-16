a73ca7012b089155d05d096b42b47915f24fdbd5
Marek <markfol89@gmail.com>
2019-10-16 17:08:14 +0200

    aktualizacja

--- src/pl/marekfoltyn/server/MainServer.java
+++ src/pl/marekfoltyn/server/MainServer.java
@@ -3,25 +3,16 @@ package pl.marekfoltyn.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainServer {
     public static void main(String[] args) throws IOException {
-        //sererSocket - aplikacja serwerowa
-        ServerSocket serverSocket = new ServerSocket(5555); //port dowolny większy od 1000
-
-        //tutaj byłby Thread zaimplementowany
-        Socket acceptedSocket = serverSocket.accept(); //w tym momencie aplikacja czeka - trzeba się przełączyć do klienta
-
-        InputStream inputStream = acceptedSocket.getInputStream(); //strumie danych
-        OutputStream outputStream = acceptedSocket.getOutputStream();
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = null;
-        while ((line = bufferedReader.readLine()) != null) {
-            System.out.println(line);
+        ServerSocket serverSocket = new ServerSocket(5555);
+        ExecutorService executorService = Executors.newCachedThreadPool();//zamiast pisać Thread najlepiej posługiwać się Executorami
+        Socket acceptedSocket = serverSocket.accept();
+        while (true) {
+            executorService.submit(new ServerConnection(acceptedSocket));
         }
-        inputStream.close();
-        outputStream.close();
     }
-
-    //W serwerze zapiąc wątki i zaimplementować Runnable
 }
--- src/pl/marekfoltyn/server/ServerConnection.java
+++ src/pl/marekfoltyn/server/ServerConnection.java
@@ -1,8 +1,31 @@
 package pl.marekfoltyn.server;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ServerConnection implements Runnable {
+    private final Socket acceptedSocket;
+
+    //Dodanie konstruktora na potrzeby podpięcia socketa na odpowiednim porcie
+    public ServerConnection(Socket acceptedSocket) {
+        this.acceptedSocket = acceptedSocket;
+    }
+
     @Override
     public void run() {
-
+        InputStream inputStream = null;
+        try {
+            inputStream = acceptedSocket.getInputStream();
+            OutputStream outputStream = acceptedSocket.getOutputStream();
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
