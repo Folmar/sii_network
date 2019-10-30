f4b60976908150e08ac656c69efd1837e1caea7b
Marek <markfol89@gmail.com>
2019-10-30 17:03:23 +0100

    aktualizacja klasy Rate

--- src/pl/marekfoltyn/nbp/Rate.java
+++ src/pl/marekfoltyn/nbp/Rate.java
@@ -1,4 +1,42 @@
 package pl.marekfoltyn.nbp;
 
+import java.math.BigDecimal;
+
 public class Rate {
+    private String currency;
+    private String code;
+    private BigDecimal mid;
+
+    public Rate(String currency, String code, BigDecimal mid) {
+        this.currency = currency;
+        this.code = code;
+        this.mid = mid;
+    }
+
+    public Rate() {
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getMid() {
+        return mid;
+    }
+
+    public void setMid(BigDecimal mid) {
+        this.mid = mid;
+    }
 }
